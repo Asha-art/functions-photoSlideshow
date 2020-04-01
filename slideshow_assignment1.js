8700000575509135718aa8f90fbcce07dc8a3419
Asha-art <62064699+Asha-art@users.noreply.github.com>
2020-04-01 15:12:19 -0400

    Add files slideshow

---
+++ slideshow_assignment1.js
@@ -0,0 +1,67 @@
+
+var slideshow = {
+
+  // 1. An array called `photoList` that contains the names of the photos as strings
+   photoList : ['photo1', 'photo2', 'photo3'] ,
+  // An integer `currentPhotoIndex` that represents which photo in the `photoList` is currently displayed 
+   currentPhotoIndex : 0,
+  
+
+
+
+  // console.log(indexof(currentPhotoIndex));
+      nextPhoto : function(){
+        
+        if(this.currentPhotoIndex < this.photoList.length){
+               
+                console.log("next photo: "+this.photoList[this.currentPhotoIndex]);
+                this.currentPhotoIndex++; 
+            }     
+
+        else{
+           // console.log("End of Slideshow");  //  Otherwise, log "End of slideshow";
+           this.pause();
+          }
+
+      },
+      
+      
+       prevPhoto : function(){
+
+        if(this.currentPhotoIndex >0){
+         
+               this.currentPhotoIndex--;  
+               console.log(this.photoList[this.currentPhotoIndex]);
+           }
+        else{
+        console.log("Start of Slideshow");
+        }
+
+      },
+      
+
+   getCurrentPhoto:function(){
+
+        return this.photoList[this.currentPhotoIndex];
+     },
+     
+
+}
+
+
+
+
+slideshow.nextPhoto();
+slideshow.nextPhoto();
+slideshow.nextPhoto();
+slideshow.prevPhoto();
+slideshow.prevPhoto();
+slideshow.prevPhoto();
+slideshow.getCurrentPhoto();
+
+
+
+
+
+
+
